eaee8d88dc1ca53f80d0914478f66e54978855c5
Hugo Santos <hugo@namespacelabs.com>
2025-06-13 17:53:22 -0700

    Make sure we lean on iad for capacity. (#5)

--- src/scripts/setup_builder.sh
+++ src/scripts/setup_builder.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# CircleCI runs out of us-east-1; lets be close.
+export NSC_ENDPOINT=https://compute.iad.namespaceapis.com
+
 if [ "<< parameters.use_as_default >>" = "true" ]; then
     nsc docker buildx setup --name namespace --use
 else
