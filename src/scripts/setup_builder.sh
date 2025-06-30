b2729934d0eb2a03ca27f6a91a544ec057de4b25
Hugo Santos <hugo@namespacelabs.com>
2025-06-30 17:07:39 +0200

    allow setting builder name and experimental settings.

--- src/scripts/setup_builder.sh
+++ src/scripts/setup_builder.sh
@@ -3,8 +3,18 @@
 # CircleCI runs out of us-east-1; lets be close.
 export NSC_ENDPOINT=https://compute.iad.namespaceapis.com
 
+CMD="nsc docker buildx setup --name namespace"
+
 if [ "<< parameters.use_as_default >>" = "true" ]; then
-    nsc docker buildx setup --name namespace --use
-else
-    nsc docker buildx setup --name namespace
+    CMD="$CMD --use"
 fi
+
+if [ -n "$BUILDER_NAME" ]; then
+    CMD="$CMD --tag $BUILDER_NAME"
+fi
+
+if [ -n "$BUILDER_EXPERIMENTAL" ]; then
+    CMD="$CMD --experimental $BUILDER_EXPERIMENTAL"
+fi
+
+$CMD
