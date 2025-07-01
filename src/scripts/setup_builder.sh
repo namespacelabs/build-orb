90dedcab01afac6e97143130207b71a03077a9fd
Niklas Gehlen <niklas@namespacelabs.com>
2025-07-01 10:37:42 +0200

    Extend e2e testing. (#9)

--- src/scripts/setup_builder.sh
+++ src/scripts/setup_builder.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+set -ex
 
 # CircleCI runs out of us-east-1; lets be close.
 export NSC_ENDPOINT=https://compute.iad.namespaceapis.com
 
 CMD="nsc docker buildx setup --name namespace"
 
-if [ "<< parameters.use_as_default >>" = "true" ]; then
+if [ "$BUILDER_USE_AS_DEFAULT" = "true" ] || [ "$BUILDER_USE_AS_DEFAULT" = 1 ]; then
     CMD="$CMD --use"
 fi
 
